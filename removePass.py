287f0146cb7449223dfdb0190813fbd1e21c47ff
JeanpieroHC <pierito2000huamani@gmail.com>
2024-03-17 01:06:20 -0500

    recibir ruta del archivo

---
+++ removePass.py
@@ -0,0 +1,13 @@
+import sys
+
+
+# Verificamos si se proporcionó la ruta
+if len(sys.argv) != 2 :
+    print("Por favor, proporciona la ruta hacia el archivo excel")
+    sys.exit(1)
+
+
+path = sys.argv[1]
+
+
+
