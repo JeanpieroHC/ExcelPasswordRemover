51948140bd503f48c2ee5ce8ebeb952f6a0df247
JeanpieroHC <pierito2000huamani@gmail.com>
2024-03-17 01:43:09 -0500

    descomprimir archivo

--- removePass.py
+++ removePass.py
@@ -1,4 +1,6 @@
+import os
 import sys
+import zipfile
 
 
 # Verificamos si se proporcionó la ruta
@@ -8,6 +10,17 @@ if len(sys.argv) != 2 :
 
 
 path = sys.argv[1]
+directorioArchivo=os.path.dirname(path)
 
+def descomprimir_archivo(archivo):
+    # Ruta del directorio donde se encuentra el archivo
 
-
+    
+    # Abre el archivo 
+    with zipfile.ZipFile(archivo, 'r') as zip_ref:
+        # Extrae todos los archivos al directorio
+        zip_ref.extractall(directorioArchivo+"/excelArchives")
+    
+    print(f"Se ha descomprimido correctamente {archivo}")
+    
+descomprimir_archivo(path)
